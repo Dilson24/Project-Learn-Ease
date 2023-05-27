18d02840f45d80f324590cc68d12d50ecfbda2a3
Dilson24 <dilacn97@hotmail.com>
2023-05-26 20:39:59 -0500

    [NEW]-add content main & scroll reveal

--- Public/Js/Home/Landigpage/index.js
+++ Public/Js/Home/Landigpage/index.js
@@ -49,3 +49,94 @@ hamburger.onclick = () => {
 
 
 
+//scroll reveal
+
+// Espera a que el contenido HTML esté cargado
+document.addEventListener('DOMContentLoaded', function (event) {
+  // Inicializa ScrollReveal con las opciones deseadas
+  const sr = ScrollReveal({
+    duration: 1000,
+    delay: 200,
+    origin: 'bottom',
+    distance: '20px',
+    easing: 'ease-in-out',
+    mobile: true
+  });
+
+  // Función para aplicar el efecto ScrollReveal a una clase específica
+  function applyScrollReveal(className) {
+    sr.reveal(className, {
+      interval: 200, // Retraso entre la aparición de cada elemento
+      reset: true // Reinicia las animaciones cuando el usuario vuelva a hacer scroll
+    });
+  }
+
+  // Aplica el efecto ScrollReveal a las clases deseadas
+  applyScrollReveal('.contentHeader');
+  applyScrollReveal('.features');
+  applyScrollReveal('.featuresContent');
+  applyScrollReveal('.FQA');
+  // También puedes aplicarlo a otras clases llamando la función applyScrollReveal con la clase correspondiente
+  // applyScrollReveal('.otraClase');
+});
+
+//acordion
+(function () {
+  var d = document,
+    accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+    setAria,
+    setAccordionAria,
+    switchAccordion,
+    touchSupported = ('ontouchstart' in window),
+    pointerSupported = ('pointerdown' in window);
+
+  skipClickDelay = function (e) {
+    e.preventDefault();
+    e.target.click();
+  }
+
+  setAriaAttr = function (el, ariaType, newProperty) {
+    el.setAttribute(ariaType, newProperty);
+  };
+  setAccordionAria = function (el1, el2, expanded) {
+    switch (expanded) {
+      case "true":
+        setAriaAttr(el1, 'aria-expanded', 'true');
+        setAriaAttr(el2, 'aria-hidden', 'false');
+        break;
+      case "false":
+        setAriaAttr(el1, 'aria-expanded', 'false');
+        setAriaAttr(el2, 'aria-hidden', 'true');
+        break;
+      default:
+        break;
+    }
+  };
+  //function
+  switchAccordion = function (e) {
+    console.log("triggered");
+    e.preventDefault();
+    var thisAnswer = e.target.parentNode.nextElementSibling;
+    var thisQuestion = e.target;
+    if (thisAnswer.classList.contains('is-collapsed')) {
+      setAccordionAria(thisQuestion, thisAnswer, 'true');
+    } else {
+      setAccordionAria(thisQuestion, thisAnswer, 'false');
+    }
+    thisQuestion.classList.toggle('is-collapsed');
+    thisQuestion.classList.toggle('is-expanded');
+    thisAnswer.classList.toggle('is-collapsed');
+    thisAnswer.classList.toggle('is-expanded');
+
+    thisAnswer.classList.toggle('animateIn');
+  };
+  for (var i = 0, len = accordionToggles.length; i < len; i++) {
+    if (touchSupported) {
+      accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+    }
+    if (pointerSupported) {
+      accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+    }
+    accordionToggles[i].addEventListener('click', switchAccordion, false);
+  }
+})();
