e77a6c360b6ded5ffdcd71d1a3e8b74d791cb30f
Dilson24 <dilacn97@hotmail.com>
2023-06-04 22:04:07 -0500

    [UPD]- update styles for login

--- App/View/User/login.php
+++ App/View/User/login.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../Public/Css/Home/Login/login.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Login</title>
 </head>
 
@@ -15,34 +16,70 @@
         <div class="login">
             <form class="form">
                 <label for="chk" aria-hidden="true">Iniciar Sesion</label>
-                <div class="inputBox">
-                    <input type="text" required="required">
-                    <span class="user">Username</span>
+                <div class="inputBoxSignIn email">
+                    <input type="email" required="required">
+                    <span>Email</span>
                 </div>
-                <div class="inputBox">
+                <div class="inputBoxSignIn password">
                     <input type="password" required="required">
-                    <span>Password</span>
+                    <span>Contraseña</span>
                 </div>
 
-                <button class="enter">Enter</button>
+                <button class="SignInBtn">Iniciar Sesion</button>
             </form>
         </div>
         <div class="register">
             <form class="form">
                 <label for="chk" aria-hidden="true">Registrarse</label>
-                <div class="inputBox1">
-                    <input type="text" required="required">
-                    <span class="user">Email</span>
+                <div class="inputBoxSignUp email2">
+                    <input type="email" required="required">
+                    <span>Email</span>
                 </div>
-                <div class="inputBox">
+                <div class="inputBoxSignUp name">
                     <input type="text" required="required">
-                    <span>Username</span>
+                    <span>Nombre</span>
                 </div>
-                <div class="inputBox">
+                <div class="inputBoxSignUp lastName">
+                    <input type="text" required="required">
+                    <span>Apellido</span>
+                </div>
+                <div class="inputBoxSignUp dateofBirth">
+                    <input id="boxdate"class="date" type="date" required="required" placeholder="Fecha de nacimiento">
+                    <span>Fecha de nacimiento</span>
+                </div>
+                <div class="inputBoxSignUp phone">
+                    <input type="tel" id="phone" name="phone" required>
+                    <span>Número de telefono</span>
+                </div>
+                <div class="inputBoxSignUp">
+                    <select id="country" class='form-control'>
+                        <option value="">-- País --</option>
+                    </select>
+                </div>
+                <div class="inputBoxSignUp">
+                    <select id="region" class='form-control' required="required">
+                        <option value="">-- Región --</option>
+                    </select>
+                </div>
+                <div class="inputBoxSignUp">
+                    <select id="city" class='form-control' required="required">
+                        <option value="">-- Ciudad --</option>
+                    </select>
+                </div>
+                <div class="inputBoxSignUp">
+                    <select id="student_type" class='form-control' required="required">
+                        <option value="">-- ¿Qué tipo de estudiante eres? --</option>
+                        <option value="">Estudiante universitario/a</option>
+                        <option value="">Estudiante de segundaria</option>
+                        <option value="">Estudiante tecnico o tecnologo</option>
+                    </select>
+                </div>
+
+                <div class="inputBoxSignUp password2">
                     <input type="password" required="required">
-                    <span>Password</span>
+                    <span>Contraseña</span>
                 </div>
-                <button class="enter">Enter</button>
+                <button class="SignUpBtn">Registrarse</button>
             </form>
         </div>
     </div>
--- Public/Js/Home/Login/login.js
+++ Public/Js/Home/Login/login.js
@@ -3,20 +3,133 @@ document.addEventListener("DOMContentLoaded", function () {
     const registerParam = urlParams.get('register');
     const chk = document.getElementById('chk');
     const originalTitle = document.title;
+    const mainElement = document.querySelector('.main');
+    const bodyElement = document.body;
 
     if (registerParam === 'newUser') {
         chk.checked = true;
         document.title = 'Register';
+        mainElement.style.maxHeight = '790px';
+        mainElement.style.margin ='0.5em 0 0.5em 0';
+        bodyElement.style.height = 'auto';
+
     }
 
     chk.addEventListener('click', function () {
         if (chk.checked) {
             history.pushState(null, '', '?register=newUser');
             document.title = 'Register';
+            mainElement.style.maxHeight = '790px';
+            mainElement.style.margin ='0.5em 0 0.5em 0';
+            bodyElement.style.height = 'auto';
         } else {
             history.pushState(null, '', window.location.pathname);
             document.title = originalTitle;
+            mainElement.style.maxHeight = '350px';
+            mainElement.style.margin ='auto 0 auto 0';
+            bodyElement.style.height = '100vh';
         }
+    });
+});
+
+/*Input controls*/
+const dateInput = document.querySelector('#boxdate');
+
+dateInput.addEventListener('click', function () {
+    this.classList.remove('date');
+});
+
+dateInput.addEventListener('blur', function () {
+    if (this.value === '') {
+        this.classList.add('date');
+    }
+});
+
+
+
+
+
+/*Test*/
+$(document).ready(function () {
+    //-------------------------------SELECT CASCADING-------------------------//
+    var selectedCountry = (selectedRegion = selectedCity = "");
+    // This is a demo API key for testing purposes. You should rather request your API key (free) from http://battuta.medunes.net/
+    var BATTUTA_KEY = "00000000000000000000000000000000";
+    // Populate country select box from battuta API
+    url =
+        "https://battuta.medunes.net/api/country/all/?key=" +
+        BATTUTA_KEY +
+        "&callback=?";
+
+    // EXTRACT JSON DATA.
+    $.getJSON(url, function (data) {
+        console.log(data);
+        $.each(data, function (index, value) {
+            // APPEND OR INSERT DATA TO SELECT ELEMENT.
+            $("#country").append(
+                '<option value="' + value.code + '">' + value.name + "</option>"
+            );
+        });
+    });
+    // Country selected --> update region list .
+    $("#country").change(function () {
+        selectedCountry = this.options[this.selectedIndex].text;
+        countryCode = $("#country").val();
+        // Populate country select box from battuta API
+        url =
+            "https://battuta.medunes.net/api/region/" +
+            countryCode +
+            "/all/?key=" +
+            BATTUTA_KEY +
+            "&callback=?";
+        $.getJSON(url, function (data) {
+            $("#region option").remove();
+            $('#region').append('<option value="">Please select your region</option>');
+            $.each(data, function (index, value) {
+                // APPEND OR INSERT DATA TO SELECT ELEMENT.
+                $("#region").append(
+                    '<option value="' + value.region + '">' + value.region + "</option>"
+                );
+            });
+        });
+    });
+    // Region selected --> updated city list
+    $("#region").on("change", function () {
+        selectedRegion = this.options[this.selectedIndex].text;
+        // Populate country select box from battuta API
+        countryCode = $("#country").val();
+        region = $("#region").val();
+        url =
+            "https://battuta.medunes.net/api/city/" +
+            countryCode +
+            "/search/?region=" +
+            region +
+            "&key=" +
+            BATTUTA_KEY +
+            "&callback=?";
+        $.getJSON(url, function (data) {
+            console.log(data);
+            $("#city option").remove();
+            $('#city').append('<option value="">Please select your city</option>');
+            $.each(data, function (index, value) {
+                // APPEND OR INSERT DATA TO SELECT ELEMENT.
+                $("#city").append(
+                    '<option value="' + value.city + '">' + value.city + "</option>"
+                );
+            });
+        });
+    });
+    // city selected --> update location string
+    $("#city").on("change", function () {
+        selectedCity = this.options[this.selectedIndex].text;
+        $("#location").html(
+            "Locatation: Country: " +
+            selectedCountry +
+            ", Region: " +
+            selectedRegion +
+            ", City: " +
+            selectedCity
+        );
     });
 });
 
