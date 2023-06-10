9905ccfccdc7814560fbe45b02f10c683438df30
Dilson24 <dilacn97@hotmail.com>
2023-06-09 22:10:35 -0500

    [NEW] admin panel

---
+++ App/View/Admin/profileAdmin.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../../Public/Css/Includes/all.css" type="text/css">
+    <link href="../../../Public/Css/Home/Landigpage/hamburgers.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../Public/Css/System/Admin/admin.css">
+    <title>Panel Admin</title>
+</head>
+
+<body>
+    <div class="sidebar active">
+        <div class="logo_content">
+            <div class="logo">
+                <img class="logoHeader" src="../../../Public/Assets/Img/headerLogo.svg" alt="header logo">
+            </div>
+            <!-- <i class='bx bx-menu-alt-right' id="btn" style="font-size: 25px;"></i> -->
+            <button id="btn" class="hamburger hamburger--arrow is-active" type="button" aria-label="Menu"
+                aria-controls="navigation">
+                <span class="hamburger-box boxNav">
+                    <span class="hamburger-inner barsNav"></span>
+                </span>
+            </button>
+        </div>
+        <ul class="nav_list">
+            <li>
+
+                <i class='bx bx-search'></i>
+                <input type="text" placeholder="Search...">
+                <span class="tooltip">Search</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="link_names">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-user'></i>
+                    <span class="link_names">User</span>
+                </a>
+                <span class="tooltip">User</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-chat'></i>
+                    <span class="link_names">Messages</span>
+                </a>
+                <span class="tooltip">Messages</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-pie-chart-alt-2'></i>
+                    <span class="link_names">Analytics</span>
+                </a>
+                <span class="tooltip">Analytics</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-folder'></i>
+                    <span class="link_names">Files</span>
+                </a>
+                <span class="tooltip">Files</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-cart-alt'></i>
+                    <span class="link_names">Orders</span>
+                </a>
+                <span class="tooltip">Orders</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-heart'></i>
+                    <span class="link_names">Liked</span>
+                </a>
+                <span class="tooltip">Liked</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-cog'></i>
+                    <span class="link_names">Settings</span>
+                </a>
+                <span class="tooltip">Settings</span>
+            </li>
+        </ul>
+        <div class="profile_content">
+            <div class="profile">
+                <div class="profile_details">
+                    <img src="https://vz.cnwimg.com/wp-content/uploads/2014/01/alex.jpg?x86007" alt="">
+                    <div class="name_job">
+                        <div class="name">Name</div>
+                        <div class="job">Role</div>
+                    </div>
+                </div>
+                <i class='bx bx-log-out' id="log_out"></i>
+            </div>
+        </div>
+    </div>
+    <div class="home_content">
+        <div class="text">Home Content Here...</div>
+    </div>
+    <script>
+        let btn = document.querySelector("#btn");
+        let sidebar = document.querySelector(".sidebar");
+        let searchBtn = document.querySelector(".bx-search");
+        let hamburger = document.querySelector(".hamburger");
+
+        btn.onclick = function () {
+            hamburger.classList.toggle("is-active");
+            sidebar.classList.toggle("active");
+        }
+        searchBtn.onclick = function () {
+            sidebar.classList.toggle("active");
+        }
+    </script>
+
+</body>
+
+</html>
