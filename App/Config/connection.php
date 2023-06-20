d2d697e4586df8a0fc2c12cd683f5c037b368733
Dilson24 <dilacn97@hotmail.com>
2023-06-20 15:36:38 -0500

    [UPD] correction to support special characters

--- App/Config/connection.php
+++ App/Config/connection.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 require_once '../../../App/config.php';
 
 try {
-    $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME.";charset=utf8", DB_USERNAME, DB_PASSWORD);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("set names utf8");
 } catch(PDOException $e) {
     throw new Exception('Database connection failed: '.$e->getMessage());
 }
-
 ?>
